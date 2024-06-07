87b5c2243866f9f1c2a4e07d7e5f754861599c96
Kepler <kyplerr@gmail.com>
2024-06-07 03:37:25 -0700

    Imported project config and discord.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import config from "../config.json";
+import Discord from "discord.js";
+
+const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
+
 console.log("Hello, Kepper!");
