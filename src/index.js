07eefe24eba5ddd36d1ca4bd78b50627b466c643
Kepler <kyplerr@gmail.com>
2024-06-07 05:01:54 -0700

    Uncapitalize Discord variable name.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import config from "../config.json" with { type: "json" };
-import Discord from "discord.js";
+import discord from "discord.js";
 
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
+const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 
-client.once(Discord.Events.ClientReady, (readyClient) => {
+client.once(discord.Events.ClientReady, (readyClient) => {
 	console.log(`Discord client ready. Logged in as ${readyClient.user.tag}`);
 });
 
