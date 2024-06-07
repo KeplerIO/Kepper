c547083273f59a3d7660fff6c543a95cdfe3b5c3
Kepler <kyplerr@gmail.com>
2024-06-07 06:54:56 -0700

    Added all bot intents and message listener. Bot replies to pings.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import config from "../config.json" with { type: "json" };
 import discord from "discord.js";
 
-const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
+const client = new discord.Client({
+	intents: Object.keys(discord.GatewayIntentBits).map((x) => { return discord.GatewayIntentBits[x]; })
+});
+
+client.login(config.auth.BOT_TOKEN);
 
 client.once(discord.Events.ClientReady, (readyClient) => {
 	console.log(`Discord client ready. Logged in as ${readyClient.user.tag}`);
 });
 
-client.login(config.auth.BOT_TOKEN);
+client.on("messageCreate", (msg) => {
+	console.log(`Message sender ID: ${msg.author.id}`);
+
+	if (msg.author.bot) {
+		return;	
+	}
+
+	if(msg.mentions.users.has(client.user.id)) {
+		msg.reply("Beep").then(() => console.log(`Replied to message "${msg.content}"`)).catch(console.error);
+	}
+
+});
