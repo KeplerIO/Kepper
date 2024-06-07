a13d5fbd4e3404f1347721850cf61278d5a8e7ca
Kepler <kyplerr@gmail.com>
2024-06-07 03:50:09 -0700

    Created Discord client and log in with bot token.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import Discord from "discord.js";
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
-console.log("Hello, Kepper!");
+client.once(Discord.Events.ClientReady, (readyClient) => {
+	console.log(`Discord client ready. Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(config.auth.BOT_TOKEN);
